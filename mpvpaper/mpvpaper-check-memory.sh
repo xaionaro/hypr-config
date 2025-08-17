7de323843627803655cf112cd4b51f6a5cf09234
Dmitrii Okunev <xaionaro@dx.center>
2025-08-17 15:06:51 +0100

    update 1755439611

---
+++ mpvpaper/mpvpaper-check-memory.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+THRESHOLD_KB="$(( 1024 * 1024 ))"
+
+ps --no-headers -C mpvpaper -o pid,rss= | \
+while read -r PID RSS; do
+	if [ "$RSS" -lt "$THRESHOLD_KB" ]; then
+		continue
+	fi
+	MPVPAPER_SUFFIX=''
+	if grep DP-2 /proc/"$PID"/cmdline; then
+		MPVPAPER_SUFFIX='-main'
+	else
+		MPVPAPER_SUFFIX='-back'
+	fi
+	systemctl --user restart mpvpaper-main.service
+done
+
