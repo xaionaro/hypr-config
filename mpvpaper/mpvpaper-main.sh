9475d33dfe5f0e985039585bb2875b0e399573cc
Dmitrii Okunev <xaionaro@dx.center>
2025-08-19 19:59:22 +0100

    update 1755629962

--- mpvpaper/mpvpaper-main.sh
+++ mpvpaper/mpvpaper-main.sh
@@ -6,6 +6,7 @@ TARGET_LENGTH=300
 
 FILENAME_PREV=''
 while true; do
+    ( pgrep swww-daemon >/dev/null || swww-daemon --format xrgb &) &
     while true; do
         FILENAME="$(find "$WALLPAPERS_PATH"/*.mp4 -type f -print0 | shuf -z -n 1 | xargs -0 basename)"
         if [[ "$FILENAME" != "$FILENAME_PREV" ]]; then
@@ -15,12 +16,16 @@ while true; do
     done
     FILEPATH="$WALLPAPERS_PATH"/"$FILENAME"
     if ! [[ -f "$FILEPATH.frame0.png" ]]; then
-        ffmpeg -i "$FILEPATH" -frames:v 1 "$FILEPATH.frame0.png"
+        ffmpeg -i "$FILEPATH" -frames:v 1 "$FILEPATH.frame0-pre.png"
+        convert "$FILEPATH.frame0-pre.png" -resize 3840x2160 "$FILEPATH.frame0.png"
+        rm -f "$FILEPATH.frame0-pre.png"
     fi
-    swww img --transition-fps 60 --transition-duration 15 --transition-type fade --outputs "$OUTPUTS" "$FILEPATH.frame0.png"
-    sleep 4 &
+    swww img --transition-fps 10 --transition-duration 5 --transition-type fade --outputs "$OUTPUTS" "$FILEPATH.frame0.png"
+    sleep 2 &
     if ! [[ -f "$FILEPATH.frame_eof.png" ]]; then
-        ffmpeg -sseof -1 -i "$FILEPATH" -update 1 "$FILEPATH.frame_eof.png"
+        ffmpeg -sseof -1 -i "$FILEPATH" -update 1 "$FILEPATH.frame_eof-pre.png"
+        convert "$FILEPATH.frame_eof-pre.png" -resize 3840x2160 "$FILEPATH.frame_eof.png"
+        rm -f "$FILEPATH.frame_eof-pre.png"
     fi
     if ! [[ -f "$FILEPATH.length" ]]; then
         ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$FILEPATH" > "$FILEPATH.length"
